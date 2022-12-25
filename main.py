2e4d16fb00e9b6a7ae3da3cdc790dbbed3317f24
Russo <rafamega@live.com>
2022-12-25 19:33:17 -0300

    Menu better developped.

--- main.py
+++ main.py
@@ -13,6 +13,25 @@ name = ""
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
+def banner():
+    print("""______     _            _   _           _       _       
+| ___ \   (_)          | | | |         | |     | |      
+| |_/ / __ _  ___ ___  | | | |_ __   __| | __ _| |_ ___ 
+|  __/ '__| |/ __/ _ \ | | | | '_ \ / _` |/ _` | __/ _ \ 
+| |  | |  | | (_|  __/ | |_| | |_) | (_| | (_| | ||  __/
+\_|  |_|  |_|\___\___|  \___/| .__/ \__,_|\__,_|\__\___|
+                             | |                        
+                             |_|                        """)
+def menu():
+    print("1. List of monitored products.")
+    print("2. Add product.")
+    print("3. Remove product.")
+    
+    print("0. Exit.")
+def menu1():
+    with open("list_products.txt", encoding="utf-8") as list_products:
+        for num, line in enumerate(list_products, 1):
+            print(str(num)+". "+ line)
 
 def get_info(URL,price_tag,name_tag):
     driver = webdriver.Chrome(chrome_options=chrome_options)
@@ -41,4 +60,16 @@ def write_data():
     data.write(date+","+price+"\n")
     data.close()
 
-
+exit = False
+while not exit == True:
+    banner()
+    menu()
+    ch = input()
+    if ch == "1":
+        menu1()
+    elif ch == "2":
+        new_product = input("Please, enter the location of the file incluiding the file itself: ")
+    elif ch == "3":
+        #for linha in "list_products.txt":
+            #print(str(linha+1)+ ". " + list_products.readline())
+        del_product = input("Which of the products should be deleted?")
